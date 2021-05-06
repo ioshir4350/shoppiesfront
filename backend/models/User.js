02a53b3193133890c7ce410a2d74bb3a663c80e8
Isfar Oshir <iao233@nyu.edu>
2021-05-06 13:43:41 -0400

    launch 1.0

--- backend/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const userSchema = new mongoose.Schema({
-    ipAddress: {type: String, required: true},
-    nominations: { type: Array }
-  });
-
-  module.exports = mongoose.model('User', userSchema)
